2f68f859a362273c09ff4bcb40e0df83885cabe3
lilvina <davinaleo2007@yahoo.com>
2019-06-18 23:11:51 -0700

    created stack class for ancestor

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,8 +1,23 @@
 # approach it as a depth-first problem
-# 
+# we have a graph of relationships between parents and children over generations
+# each individual is assigned a unique integer identifier
+# return the earliest known ancestor, from the farthest distance from the input
+# if one or more ancestor tied for the earliest, then return the one with the lowest numeric ID
+# return -1 if the input individual that has no parents
 
 class Stack:
     def __init__(self):
-        self.storage = []
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
 
-    
+        
+
+

8a1022df1d829cbe58e8fbb71c19cbacfa2a4ced
lilvina <davinaleo2007@yahoo.com>
2019-06-27 15:19:47 -0700

    redo social graph project

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,13 +1,14 @@
 import random
+import math
+import time
 
 class Queue:
     def __init__(self):
         self.queue = []
-
     def enqueue(self, value):
         self.queue.append(value)
     def dequeue(self):
-        if self.size() > 0:
+        if (self.size) != 0:
             return self.queue.pop(0)
         else:
             return None
@@ -17,8 +18,6 @@ class Queue:
 class User:
     def __init__(self, name):
         self.name = name
-    def __repr__(self):
-        self.name
 
 class SocialGraph:
     def __init__(self):
@@ -27,94 +26,106 @@ class SocialGraph:
         self.friendships = {}
 
     def addFriendship(self, userID, friendID):
-        """
-        Creates a bi-directional friendship
-        """
         if userID == friendID:
-            print("WARNING: You cannot be friends with yourself")
+            pass
         elif friendID in self.friendships[userID] or userID in self.friendships[friendID]:
-            print("WARNING: Friendship already exists")
+            pass
         else:
             self.friendships[userID].add(friendID)
             self.friendships[friendID].add(userID)
+            return True
 
     def addUser(self, name):
-        """
-        Create a new user with a sequential integer ID
-        """
-        self.lastID += 1  # automatically increment the ID to assign the new user
+        self.lastID += 1
         self.users[self.lastID] = User(name)
         self.friendships[self.lastID] = set()
 
     def populateGraph(self, numUsers, avgFriendships):
-        """
-        Takes a number of users and an average number of friendships
-        as arguments
-
-        Creates that number of users and a randomly distributed friendships
-        between those users.
-
-        The number of users must be greater than the average number of friendships.
-        """
-        # Reset graph
         self.lastID = 0
         self.users = {}
         self.friendships = {}
-        # !!!! IMPLEMENT ME
 
-        # Add users
-        for i in range(0, numUsers):
-            self.addUser(f"User {i}")
-        # Create friendships
+        for i in range(numUsers):
+            self.addUser(f"User {i + 1}")
+
         possibleFriendships = []
-        # generate all possible friendship combinations
-        # avoid duplicates by ensureing the first number is smaller than the second
         for userID in self.users:
             for friendID in range(userID + 1, self.lastID + 1):
                 possibleFriendships.append((userID, friendID))
-        # shuffle the possible friendships
         random.shuffle(possibleFriendships)
-        # create friendships for the first X pairs of the list
-        # X is determined by the formula: numUsers * avgFriendships
-        for i in range(numUsers * avgFriendships // 2):
+        for i in range(0, (numUsers * avgFriendships // 2)):
             friendship = possibleFriendships[i]
             self.addFriendship(friendship[0], friendship[1])
 
+    def populateGraphLinear(self, numUsers, avgFriendships):
+        self.lastID = 0
+        self.users = {}
+        self.friendships = {}
+
+        for i in range(numUsers):
+            self.addUser(f"User {i + 1}")
+        
+        targetFriendships = (numUsers * avgFriendships) // 2
+        totalFriendships = 0
+        collisions = 0
+
+        while totalFriendships < targetFriendships:
+            userID = random.randint(1, self.lastID)
+            friendID = random.randint(1, self.lastID)
+            if self.addFriendship(userID, friendID):
+                totalFriendships += 2
+            else:
+                collisions += 1
+        print(f"COLLISIONS: {collisions}")
+
     def getAllSocialPaths(self, userID):
-        """
-        Takes a user's userID as an argument
-
-        Returns a dictionary containing every user in that user's
-        extended network with the shortest friendship path between them.
-
-        The key is the friend's ID and the value is the path.
-        """
-        visited = {}  # Note that this is a dictionary, not a set
-        # !!!! IMPLEMENT ME
-        # create an empty queue and enqueue
-        # while the size is empty
-        # dequeue the path
-        # if the vertex hasn't been visited
-        # mark as visited
-        # add all the neighbors to the queue
+        visited = {}
         q = Queue()
         q.enqueue([userID])
-        while q.size() > 0:
+        while q.size() != 0:
             path = q.dequeue()
-            v = path[-1]
-            if v not in visited:
-                visited[v] = path
-                for neighbor in self.friendships[v]:
-                    copy_path = list(path)
-                    copy_path.append(neighbor)
-                    q.enqueue(copy_path)
-
+            newUserID = path[-1]
+            if newUserID not in visited:
+                visited[newUserID] = path
+                for friendID in self.friendships[newUserID]:
+                    if friendID not in visited:
+                        new_path = list(path)
+                        new_path.append(friendID)
+                        q.enqueue(new_path)
         return visited
-
 
 if __name__ == '__main__':
     sg = SocialGraph()
-    sg.populateGraph(10, 2)
-    print(sg.friendships)
-    connections = sg.getAllSocialPaths(1)
-    print(connections)
+    totalConnections = 0
+    totalDegrees = 0
+    iterations = 10
+
+    for i in range(0, iterations):
+        sg.populateGraph(1000, 5)
+        connections = sg.getAllSocialPaths(1)
+        total = 0
+        for userID in connections:
+            total += len(connections[userID]) - 1
+
+        totalConnections += len(connections)
+        totalDegrees += total / len(connections)
+
+        print("------------")
+        print(f"Friends in network: {len(connections)}")
+        print(f"Average degrees: {total / len(connections)}")
+
+    print(f"Average Connections: {totalConnections / iterations}")
+    print(f"Average Degrees: {totalDegrees / iterations}")
+
+if __name__ == '__main__':
+    sg = SocialGraph()
+    numUsers = 50000
+    avgFriendships = 100
+    linear_start_time = time.time()
+    sg.populateGraphLinear(numUsers, avgFriendships)
+    linear_end_time = time.time()
+    print(f"Linear runtime: {linear_end_time - linear_start_time}")
+    quad_start_time = time.time()
+    sg.populateGraph(numUsers, avgFriendships)
+    quad_end_time = time.time()
+    print(f"Quadratic runtime: {quad_end_time - quad_start_time}")
